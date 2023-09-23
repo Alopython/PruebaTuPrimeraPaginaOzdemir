b0e55ad3d906117b1705b2db73344300d1b05691
Alo <alo@gmail>
2023-09-23 17:48:27 -0300

    Delete app myapp

--- myproject/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-
-# Create your views here.
-
-def index(request):
-    contexto = {"nombre": "Reliability & Maintenance"}
-    return render(request, "myapp/index.html",contexto)
